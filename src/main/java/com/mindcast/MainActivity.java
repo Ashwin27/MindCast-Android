3dedfffcfae1d9b0aaf5c800d6ebff12618b90f2
Ashwin Venkatesh Raman <ashwinrmn@gmail.com>
2013-11-24 23:17:32 -0500

    Working code

--- src/main/java/com/mindcast/MainActivity.java
+++ src/main/java/com/mindcast/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.fragment_main);
+        setContentView(R.layout.activity_main);
         statusFeed = new StatusFeed();
 
 
--- src/main/java/com/mindcast/StatusFeed.java
+++ src/main/java/com/mindcast/StatusFeed.java
@@ -8,17 +8,38 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.view.View;
 
+import java.util.List;
+
 /**
  * Created by Ashwin on 11/24/13.
  */
 public class StatusFeed {
 
-    public StatusFeed(){
+    private List<Status> statusList;
 
+    public StatusFeed(){
+        getStatuses();
     }
 
-    private Status getStatuses(){
-        return new Status("SampleUser", "SampleStatus");
+    private boolean getStatuses(){
+        // TODO: Create HttpRequest object here and pass it to populateStatusList()
+        boolean success = populateStatusList();
+
+        return success;
+    }
+
+    private boolean populateStatusList(){
+        boolean success = true;
+
+        try{
+            statusList.add( new Status("SampleUser", "SampleStatus") );
+        }
+
+        catch(Exception ex){
+            success = false;
+        }
+
+        return success;
     }
 
 
